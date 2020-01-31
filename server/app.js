18cf73f8e391180deaf50ac63f3c34ceeb5fd1b6
eveseow <49668778+eveseow@users.noreply.github.com>
2020-01-31 13:18:41 +0800

    changes no.3

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const path = require('path');
 
 var bodyParser = require('body-parser');
